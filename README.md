5f7c1562065005cfd8172776f0625a51d16393e9
Sakura Blossom <sakurablossom@Sakuras-MacBook-Pro.local>
2020-11-27 20:58:37 -0800

    Updated my readme and index.js. Added files in my view folder. Connected fontawesome, bootstrap CDN to style my pages.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Inventory-System
 Event and inventory system management application are you to see previous orders add a new request orders and set the pricing of product.
+
+Tools I used:
+bootswatch, 
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,6 @@ and dashboard things
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('welcome'));
+router.get('/', (req, res) => res.render('welcome'));
 
 module.exports = router;
