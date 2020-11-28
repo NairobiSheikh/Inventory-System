a9e950a8cf00527929cd2ca9d2e5542b70715a82
Sakura Blossom <sakurablossom@Sakuras-MacBook-Pro.local>
2020-11-27 23:17:37 -0800

    Created is it a JS file inside my models contains my shema. updated my server.js and users.js in my routes folder etc...

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 app.use(expressLoyouts);
 app.set('view engine', 'ejs');
 
+//Bodyparser
+app.use(express.urlencoded({ extended: false }));
+
 //Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
