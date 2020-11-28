8fc4566e81b75674932802588568d310db916a0c
Sakura Blossom <sakurablossom@Sakuras-MacBook-Pro.local>
2020-11-27 23:59:04 -0800

    Something broke I'm ah call it night and pick this back up tomorrow

--- routes/users.js
+++ routes/users.js
@@ -10,34 +10,34 @@ router.get('/register', (req, res) => res.render('register'));
 
 // Register Handle
 router.post('/register', (req, res) => {
-  console.log(req.body)
-  res.send('Hello World!!');
-  // const { name, email, password, password2 } = req.body;
-  // let errors = [];
+  // console.log(req.body)
+  // res.send('Hello World!!');
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
 
-  // if (!name || !email || !password || !password2) {
-  //   errors.push({ msg: 'Please enter all fields' });
-  // }
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please enter all fields' });
+  }
 
-  // if (password != password2) {
-  //   errors.push({ msg: 'Passwords do not match' });
-  // }
+  if (password != password2) {
+    errors.push({ msg: 'Passwords do not match' });
+  }
 
-  // if (password.length < 6) {
-  //   errors.push({ msg: 'Password must be at least 6 characters' });
-  // }
+  if (password.length < 6) {
+    errors.push({ msg: 'Password must be at least 6 characters' });
+  }
 
-  // if (errors.length > 0) {
-  //   res.render('register', {
-  //     errors,
-  //     name,
-  //     email,
-  //     password,
-  //     password2
-  //   });
-  // }else {
-  //   res.send('pass');
-  // }
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    });
+  }else {
+    res.send('pass');
+  }
 });
 
 module.exports = router;
